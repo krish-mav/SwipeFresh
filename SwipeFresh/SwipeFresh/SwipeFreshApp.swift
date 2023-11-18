9fb0c7b201a50ab75fc92174bc57af4ad1449c8a
Julian Kraus <>
2023-11-18 09:30:50 +0100

    Initial commit

---
+++ SwipeFresh/SwipeFresh/SwipeFreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeFreshApp.swift
+//  SwipeFresh
+//
+//  Created by Julian Kraus on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SwipeFreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
