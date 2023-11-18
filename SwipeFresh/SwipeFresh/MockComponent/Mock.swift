c3fc93078fe81838717031397ada071b4c65c254
krish-mav <mavanikrishna1728@gmail.com>
2023-11-18 16:28:30 +0100

    fodo p

--- SwipeFresh/SwipeFresh/MockComponent/Mock.swift
+++ SwipeFresh/SwipeFresh/MockComponent/Mock.swift
@@ -12,4 +12,14 @@ class Mock {
     static var recipe: Recipe = Recipe(title: "Test")
     
     static var recipeStack: [Recipe] = [recipe, recipe]
+    
+    static var allergens: [Allergen] = [
+        Allergen(name: "Fish", isSelected: false),
+        Allergen(name: "Sojabeans", isSelected: false),
+        Allergen(name: "Nuts", isSelected: false),
+        Allergen(name: "Eggs", isSelected: false),
+        Allergen(name: "Dairy free", isSelected: false),
+        Allergen(name: "Gluten free", isSelected: false)
+        
+    ]
 }
