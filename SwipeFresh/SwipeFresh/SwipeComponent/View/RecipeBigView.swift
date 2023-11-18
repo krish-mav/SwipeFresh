8f9d1eeee1178adb8006bfc46145bbec8a9ad4cd
Aleksandra Topalova <aleksandratopalova@w215-5s-v4.eduroam.dynamic.rbg.tum.de>
2023-11-18 13:41:44 +0100

    Begin recipe big view

--- SwipeFresh/SwipeFresh/SwipeComponent/View/RecipeBigView.swift
+++ SwipeFresh/SwipeFresh/SwipeComponent/View/RecipeBigView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct RecipeBigView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        Image with url
+//        Name
+//        Ingredients
+//        Tags
+//        Prep time
+//        Instructions
+//        ...Time
+        Text("")
     }
 }
 
