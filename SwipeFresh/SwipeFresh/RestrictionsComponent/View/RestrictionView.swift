484c32aa340ac9e109cdbb028057ca287ebf3ff3
krish-mav <mavanikrishna1728@gmail.com>
2023-11-19 05:03:11 +0100

    add statistic view

--- SwipeFresh/SwipeFresh/RestrictionsComponent/View/RestrictionView.swift
+++ SwipeFresh/SwipeFresh/RestrictionsComponent/View/RestrictionView.swift
@@ -70,6 +70,7 @@ struct RestrictionView: View {
                                      }*/
                                 }
                             }
+                            
                         }
                 }
             }
